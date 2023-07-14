b2abe79205faaace383b838daa65110499a7d78c
A DoG NaMeD SpoT <adognamedspot@gmail.com>
2023-07-13 17:16:57 -0700

    Initial commit

---
+++ src/main/java/com/adognamedspot/nbtgui/NBTGUI.java
@@ -0,0 +1,27 @@
+package com.adognamedspot.nbtgui;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NBTGUI extends JavaPlugin implements Listener {
+	
+    @Override
+    public void onEnable() {
+    	// TODO
+    }
+
+    @Override
+    public void onDisable() {
+    	// TODO
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	// TODO
+    	return false;
+    }
+
+	
+}
